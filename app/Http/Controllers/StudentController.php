ab2d4d267d38663f56079b260d599b586f249884
HsuMonLynn <scm.hsumonlynn@gmail.com>
2022-05-24 14:12:41 +0630

    fix session

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Session;
+use Illuminate\Support\Facades\Session;
 
 class StudentController extends Controller
 {   
@@ -14,11 +14,11 @@ class StudentController extends Controller
 
     public function store(Request $request)
     {
-        $request->session()->put('name', $request->name);
-        $request->session()->put('selectHobbie', $request->selectHobbie);
-        $request->session()->put('gender', $request->gender);
-        $request->session()->put('fav', $request->fav);
-        $request->session()->put('matrial', $request->matrial);
+        Session::put('name', $request->name);
+        Session::put('selectHobbie', $request->selectHobbie);
+        Session::put('gender', $request->gender);
+        Session::put('fav', $request->fav);
+        Session::put('matrial', $request->matrial);
 
         return redirect()->route('students.about')->with('success', 'Thank you!
         Your Form Data has been successfully sent.');
@@ -27,12 +27,13 @@ class StudentController extends Controller
 
     public function about()
     {
-        $name = session()->get('name');
-        $hobbies = session()->get('selectHobbie');
-        $gender = session()->get('gender');
-        $favs = session()->get('fav');
-        $matrial = session()->get('matrial');
+        $name = Session::get('name');
+        $hobbies = Session::get('selectHobbie');
+        $gender = Session::get('gender');
+        $favs =  Session::get('fav');
+        $matrial = Session::get('matrial');
+
         return view('student-show',
-        compact('name','hobbies','gender','favs','matrial'));
+                compact('name','hobbies','gender','favs','matrial'));
     }
 }
